9566f935cf627d581be1372f29cb918236e7725e
Patrick Bittner <patrick.bittner@hamburg.de>
2023-02-10 13:17:35 +0100

    deployment for git-hub pages

---
+++ docs/assets/basic/app-carousel/example.ts
@@ -0,0 +1,21 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+import {CarouselQuery} from "angular2-simplicity";
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: 'app-carousel.component.html',
+  styleUrls: ['app-carousel.component.css'],
+  encapsulation : ViewEncapsulation.None
+})
+export class AppCarouselComponent {
+
+  loader(query : CarouselQuery, callback : (rows : any[]) => void) {
+    fetch("assets/materials.json")
+      .then(response => response.json())
+      .then(response => {
+        let rows : any[] = response.rows;
+        callback(rows.slice(query.index, query.index + query.limit))
+      })
+  }
+
+}
