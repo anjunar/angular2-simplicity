029901424daecd1fc3619602641f5876bc560002
Patrick <patrick.bittner@hamburg.de>
2023-03-03 08:24:15 +0100

    edit: as-page with ng-if rendered page

--- projects/angular2-simplicity/src/lib/as-page/as-page.component.ts
+++ projects/angular2-simplicity/src/lib/as-page/as-page.component.ts
@@ -1,25 +1,14 @@
-import {Component, ElementRef, OnChanges, SimpleChanges, ViewEncapsulation} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'as-page',
   templateUrl: 'as-page.component.html',
   styleUrls: ['as-page.component.css'],
-  encapsulation : ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 export class AsPageComponent {
 
-  constructor(private elementRef : ElementRef) {}
+  active = false;
 
-  get active() {
-    return this.elementRef.nativeElement.style.display === "block"
-  }
-
-  set active(value) {
-    if (value) {
-      this.elementRef.nativeElement.style.display = "block"
-    } else {
-      this.elementRef.nativeElement.style.display = "none"
-    }
-  }
 
 }
